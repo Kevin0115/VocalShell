d8f45c3b9e2748df46cbccd665be670c3f0df907
Kevin0115 <kevin.choi0115@gmail.com>
2020-01-11 15:53:49 -0800

    fixes

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import Body from './components/Body';
+import VocalShell from './components/VocalShell';
 
 function App() {
   return (
     <div className="App">
+      {/* <VocalShell /> */}
       <Header />
       <Body />
     </div>
