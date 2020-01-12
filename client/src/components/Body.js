ea6b194936a5b7f2f176d3ec2af73545962bb434
Kevin0115 <kevin.choi0115@gmail.com>
2020-01-11 19:01:10 -0800

    preliminary integration

--- client/src/components/Body.js
+++ client/src/components/Body.js
@@ -1,25 +1,30 @@
 import React, { Component } from 'react';
 import { Card, Tabs, Tab } from 'react-bootstrap';
+import '../css/Body.css';
 import '../App.css';
+
+import VocalShell from './VocalShell';
 
 class Body extends Component {
   render() {
-      return(
-        <div className="body-content">
-            <Card style={{ width: '80vw', height: "80vh" }}>
-                <Card.Body>
-                    <Tabs defaultActiveKey="main">
-                        <Tab eventKey="shell" title="Shell">
-                        Shell content
-                        </Tab>
-                        <Tab eventKey="logs" title="Logs">
-                        Log content
-                        </Tab>
-                    </Tabs>
-                </Card.Body>
-            </Card>
-        </div>
-      );
+    return(
+      <div className="body-content">
+        <Card style={{ width: '80vw', height: "80vh" }}>
+          <Card.Body>
+            <Tabs defaultActiveKey="shell">
+              <Tab eventKey="shell" title="Shell">
+                <div className="vocalshell-wrapper">
+                  <VocalShell />
+                </div>
+              </Tab>
+              <Tab eventKey="logs" title="Logs">
+              Log content
+              </Tab>
+            </Tabs>
+          </Card.Body>
+        </Card>
+      </div>
+    );
   }
 }
 
