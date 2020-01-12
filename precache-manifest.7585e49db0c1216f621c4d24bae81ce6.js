c8aaf1f87b024e7485e2f7f5f2ee960c1a1b5ecf
Kevin0115 <kevin.choi0115@gmail.com>
2020-01-11 22:59:47 -0800

    Updates

---
+++ precache-manifest.7585e49db0c1216f621c4d24bae81ce6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4275f33257f3a5978324f0b3f074f58b",
+    "url": "/VocalShell/index.html"
+  },
+  {
+    "revision": "c90b51d54ef3310cbb7d",
+    "url": "/VocalShell/static/css/main.b61e1f40.chunk.css"
+  },
+  {
+    "revision": "8b83ca2621e10dbdadab",
+    "url": "/VocalShell/static/js/2.eba2f3ce.chunk.js"
+  },
+  {
+    "revision": "0dad063812af0f5d9e30d1b8bf10f9fc",
+    "url": "/VocalShell/static/js/2.eba2f3ce.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c90b51d54ef3310cbb7d",
+    "url": "/VocalShell/static/js/main.15a98e60.chunk.js"
+  },
+  {
+    "revision": "77aea9d492d7ac45047b",
+    "url": "/VocalShell/static/js/runtime-main.fb8ae425.js"
+  }
+]);
